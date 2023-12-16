43a9ce4ab1e5f1e98fe4eb83f82f09c5be44d0c2
Edward Rodriguez <36542201+Edward-Rodriguez@users.noreply.github.com>
2023-12-16 13:37:21 -0500

    Add styling to temperature header section

--- src/components/temperature-header/temperatureHeader.js
+++ src/components/temperature-header/temperatureHeader.js
@@ -1,14 +1,50 @@
 import './temperatureHeader.css';
+import { format } from 'date-fns';
 
-export default function temperatureHeader(weatherData) {
+export default function temperatureHeader(weatherData, forecastData) {
+  const weatherInfo = weatherData;
+  const forecastInfo = forecastData;
   const container = document.createElement('div');
-  const locationHeader = document.createElement('header');
-  const time = document.createElement('div');
+  const locationHeader = document.createElement('h2');
+  const time = document.createElement('p');
+  const tempDiv = document.createElement('div');
   const temperature = document.createElement('h1');
+  const tempScale = document.createElement('span');
+  const forecastDiv = document.createElement('div');
   const condition = document.createElement('div');
-  const hiLoTemps = document.createElement('div');
+  const hiTemp = document.createElement('span');
+  const loTemp = document.createElement('span');
+  const weatherIcon = document.createElement('img');
+  const celsiusDegree = '\u2103';
+  const farenheitDegree = '\u2109';
 
   container.setAttribute('id', 'temperature-header');
+  locationHeader.setAttribute('id', 'location');
+  locationHeader.textContent = `${weatherInfo.location}, ${weatherInfo.region}`;
+  time.setAttribute('id', 'date');
+  time.textContent = format(
+    new Date(weatherInfo.localtime),
+    'EEEE, do MMM yy pp',
+  );
+  tempDiv.setAttribute('id', 'temp-container');
+  temperature.textContent = weatherInfo.temperatureFarenheit;
+  temperature.setAttribute('id', 'current-temp');
+  tempScale.textContent = farenheitDegree;
+  tempScale.setAttribute('id', 'temp-scale');
+  tempDiv.append(temperature, tempScale);
+
+  // forecast div with icon, condition, hi & lo
+  condition.textContent = weatherInfo.weatherCondition;
+  condition.setAttribute('id', 'condition');
+  weatherIcon.src = weatherInfo.weatherIcon;
+  hiTemp.textContent = `Hi: ${forecastInfo.maxTemperatureFarenheit}${farenheitDegree}`;
+  hiTemp.setAttribute('id', 'hiTemp');
+  loTemp.setAttribute('id', 'loTemp');
+  loTemp.textContent = `Lo: ${forecastInfo.minTemperatureFarenheit}${farenheitDegree}`;
+  forecastDiv.setAttribute('id', 'current-forecast');
+  forecastDiv.append(weatherIcon, condition, hiTemp, loTemp);
+
+  container.append(locationHeader, time, tempDiv, forecastDiv);
 
   return container;
 }
--- src/index.js
+++ src/index.js
@@ -1,29 +1,96 @@
+/* eslint-disable wrap-iife */
 import './assets/css/styles.css';
+import temperatureHeader from './components/temperature-header/temperatureHeader';
 
-//  prettier-ignore
+// prettier=ignore
 const dataController = (function () {
   async function getCurrentWeather(location) {
     try {
-      const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=684d4e5264b04f99a1d142811231412&q=${location}`);
+      const response = await fetch(
+        `http://api.weatherapi.com/v1/current.json?key=684d4e5264b04f99a1d142811231412&q=${location}`,
+      );
       const weatherData = await response.json();
-      console.log(weatherData);
+      return weatherData;
     } catch (err) {
-      console.err(err);
+      return err;
     }
   }
 
-  async function getForecast(location) {
+  async function getForecast(location, days = 1) {
     try {
-      const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=684d4e5264b04f99a1d142811231412&q=${location}`);
+      const response = await fetch(
+        `http://api.weatherapi.com/v1/forecast.json?key=684d4e5264b04f99a1d142811231412&q=${location}&days=${days}`,
+      );
       const forecastData = await response.json();
-      console.log(forecastData);
+      return forecastData;
     } catch (err) {
-      console.err(err);
+      return err;
     }
   }
 
-  return { getCurrentWeather, getForecast };
-}());
+  function parseWeatherJson(weatherDataJson) {
+    // console.log(weatherDataJson);
+    const {
+      name: location,
+      region,
+      country,
+      localtime,
+    } = weatherDataJson.location;
+    const {
+      temp_f: temperatureFarenheit,
+      wind_mph: windSpeedMph,
+      humidity,
+      feelslike_f: feelsLikeFarenheit,
+    } = weatherDataJson.current;
+    const { text: weatherCondition, icon: weatherIcon } =
+      weatherDataJson.current.condition;
 
-dataController.getCurrentWeather('London');
-dataController.getForecast('London');
+    return {
+      location,
+      region,
+      country,
+      localtime,
+      temperatureFarenheit,
+      windSpeedMph,
+      humidity,
+      feelsLikeFarenheit,
+      weatherCondition,
+      weatherIcon,
+    };
+  }
+
+  function parseForecastJson(forecastDataJson, day = 0) {
+    const {
+      maxtemp_f: maxTemperatureFarenheit,
+      mintemp_f: minTemperatureFarenheit,
+      daily_chance_of_rain: dailyChanceOfRain,
+    } = forecastDataJson.forecast.forecastday[day].day;
+    return {
+      maxTemperatureFarenheit,
+      minTemperatureFarenheit,
+      dailyChanceOfRain,
+    };
+  }
+
+  return {
+    getCurrentWeather,
+    getForecast,
+    parseWeatherJson,
+    parseForecastJson,
+  };
+})();
+const location = '11420';
+const weatherData = dataController.getCurrentWeather(location);
+const forecastData = dataController.getForecast(location);
+
+(function displayController() {
+  const pageContainer = document.getElementById('page-container');
+
+  Promise.all([weatherData, forecastData]).then((data) => {
+    const parsedWeatherData = dataController.parseWeatherJson(data[0]);
+    const parsedForecastData = dataController.parseForecastJson(data[1]);
+    pageContainer.prepend(
+      temperatureHeader(parsedWeatherData, parsedForecastData),
+    );
+  });
+})();
